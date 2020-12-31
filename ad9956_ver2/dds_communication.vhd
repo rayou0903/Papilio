5190bc4c5fe25d0113f0075d3a9ae8e81f06e8dd
rayou0903 <rayou0903@gmail.com>
2020-12-31 16:37:57 +0900

    PS・レジスタ切り替え追加

--- ad9956_ver2/dds_communication.vhd
+++ ad9956_ver2/dds_communication.vhd
@@ -31,81 +31,94 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity dds_communication is
-	Port ( 
-		SCLK			: in		STD_LOGIC;								--シリアル通信用クロック信号(25MHz)
-		RST			: in		STD_LOGIC;								--リセット信号
-		RQ				: in		STD_LOGIC;								--リクエスト信号(1でリクエスト)
-		R_OR_W		: in		STD_LOGIC;								--読み書き識別信号(0で読み出し，1で書き込み)
-		AC_REG		: in  	STD_LOGIC_VECTOR (4 downto 0);	--アクセスレジスタ
-		DSEND			: in		STD_LOGIC_VECTOR (63 downto 0);	--送信データ
-		SDIO			: inout	STD_LOGIC;								--通信信号
-		CS 			: out		STD_LOGIC;								--チップセレクト信号(0で通信可能)
-		IO_UPDATE	: out		STD_LOGIC;								--データ更新信号(1で更新)
-		DGET			: out		STD_LOGIC_VECTOR (63 downto 0);	--受信データ
-		BUSY			: out		STD_LOGIC;								--ビジー信号(1でビジー状態)
-		TXD			: out		STD_LOGIC
-	);
+    Port ( 
+        SCLK         : in       STD_LOGIC;                         --シリアル通信用クロック信号(25MHz)
+        RST          : in       STD_LOGIC;                         --リセット信号
+        RQ           : in       STD_LOGIC;                         --リクエスト信号(1でリクエスト)
+        R_OR_W       : in       STD_LOGIC;                         --読み書き識別信号(0で読み出し，1で書き込み)
+        AC_REG       : in       STD_LOGIC_VECTOR (4 downto 0);     --アクセスレジスタ
+        DSEND        : in       STD_LOGIC_VECTOR (63 downto 0);    --送信データ
+        SDIO         : inout    STD_LOGIC;                         --通信信号
+        CS           : out      STD_LOGIC;                         --チップセレクト信号(0で通信可能)
+        IO_UPDATE    : out      STD_LOGIC;                         --データ更新信号(1で更新)
+        DGET         : out      STD_LOGIC_VECTOR (63 downto 0);    --受信データ
+        BUSY         : out      STD_LOGIC;                         --ビジー信号(1でビジー状態)
+        TXD          : out      STD_LOGIC                          --送信完了信号
+    );
 end dds_communication;
 
 architecture Behavioral of dds_communication is
-	-- state transition
-	type state_t is (IDLE, INSTRUCTION, GET, SEND, UPDATE);
-	signal state : state_t := IDLE;
+    -- state transition
+    type state_t is (IDLE, INSTRUCTION, GET, SEND, UPDATE);
+    signal state : state_t := IDLE;
 	
-	--innner signal
-	signal sdio_inner : std_logic_vector(63 downto 0) := (others => '0');	--内部SDIO
-	signal bit_cnt		: integer := 0;													--ビットカウンタ
+    --innner signal
+    signal sdio_inner    : std_logic_vector(63 downto 0) := (others => '0');    --内部SDIO
+    signal bit_cnt       : integer := 0;                                        --ビットカウンタ
+    signal bit_len       : integer := 32;                                       --送信ビット長(初期値はCFR1)
 	
 begin
+    --SDIO <= sdio_inner(63);
+    process(SCLK, RST, RQ) begin
+        --初期化
+        if(RST = '0') then
+            CS         <= '1';
+            IO_UPDATE  <= '0';
+            DGET       <= (others => '0');
+            BUSY       <= '0';
+            TXD        <= '0';
+            sdio_inner <= (others => '0');
+            bit_cnt    <= 0;
+            bit_len    <= 32;
+            state      <= IDLE;
 
-	--SDIO <= sdio_inner(63);
-	process(SCLK, RST, RQ) begin
-		--初期化
-		if(RST = '0') then
-			CS				<= '1';
-			IO_UPDATE 	<= '0';
-			DGET			<= (others => '0');
-			BUSY			<= '0';
-			TXD			<= '0';
-			sdio_inner	<= (others => '0');
-			bit_cnt		<= 0;
-			state			<= IDLE;
-			
-		elsif rising_edge(SCLK) then
-			case state is
-				when IDLE =>
-					if(RQ = '1') then
-						BUSY			<= '1';	--ビジー状態
-						--始めの8bit以外は，0で埋める
-						sdio_inner	<= not(R_OR_W) & "00" & AC_REG & (63-8 downto 0 => '0');
-						state			<= INSTRUCTION;
-					else
-						CS				<= '1';
-						SDIO			<= 'Z';
-						IO_UPDATE 	<= '0';
-						BUSY			<= '0';
-						sdio_inner 	<= (others => '0');
-					end if;
-					
-				--命令サイクル
-				when INSTRUCTION =>
-					CS		<= '0';
-					SDIO <= sdio_inner(63);
-					if(bit_cnt = 7) then
-						bit_cnt <= 0;		--ビットカウンタをリセット
-						--状態遷移(今回はすべてSENDに遷移)
-						if(R_OR_W = '0') then
-							state	<= GET;
-						elsif(R_OR_W = '1') then
-							sdio_inner <= DSEND;
-							state	<= SEND;
-						else
-							state <= IDLE;
-						end if;
-					else
-						bit_cnt   <= bit_cnt + 1;							--ビットカウンタを1増やす
-						sdio_inner <= sdio_inner(62 downto 0) & '0';	--1ビット左シフト
-					end if;
+        elsif rising_edge(SCLK) then
+            case state is
+                when IDLE =>
+                    if(RQ = '1') then
+                        BUSY <= '1';    --ビジー状態
+                        --始めの8bit以外は，0で埋める
+                        sdio_inner <= not(R_OR_W) & "00" & AC_REG & (63-8 downto 0 => '0');
+                        --ビット長の変更
+                        if(AC_REG = "00000")then
+                            bit_len <= 32;
+                        elsif(AC_REG = "00001")then
+                            bit_len <= 40;
+                        elsif(AC_REG = "00010" or AC_REG = "00011")then
+                            bit_len <= 24;
+                        elsif(AC_REG = "00100" or AC_REG = "00101")then
+                            bit_len <= 16;
+                        else
+                            bit_len <= 64;
+                        end if;
+                            state <= INSTRUCTION;
+                    else
+                        CS         <= '1';
+                        SDIO       <= 'Z';
+                        IO_UPDATE  <= '0';
+                        BUSY       <= '0';
+                        sdio_inner <= (others => '0');
+                    end if;
+
+                --命令サイクル
+                when INSTRUCTION =>
+                    CS   <= '0';
+                    SDIO <= sdio_inner(63);
+                    if(bit_cnt = 7) then
+                        bit_cnt <= 0;    --ビットカウンタをリセット
+                        --状態遷移(今回はすべてSENDに遷移)
+                        if(R_OR_W = '0') then
+                            state <= GET;
+                        elsif(R_OR_W = '1') then
+                            sdio_inner <= DSEND;
+                            state      <= SEND;
+                        else
+                            state <= IDLE;
+                        end if;
+                    else
+                        bit_cnt    <= bit_cnt + 1;                      --ビットカウンタを1増やす
+                        sdio_inner <= sdio_inner(62 downto 0) & '0';    --1ビット左シフト
+                    end if;
 				
 				--未完成
 --				when GET =>
@@ -126,27 +139,27 @@ begin
 --						clk_cnt   <= clk_cnt + 1;
 --					end if;
 					
-				--データ転送サイクル
-				when SEND =>
-					SDIO <= sdio_inner(63);
-					if(bit_cnt = 63) then
-						bit_cnt <= 0;			--ビットカウンタをリセット
-						state   <= UPDATE;
-					else
-						bit_cnt   <= bit_cnt + 1;							--ビットカウンタを1増やす
-						sdio_inner <= sdio_inner(62 downto 0) & '0';	--1ビット左シフト
-					end if;
+                --データ転送サイクル
+                when SEND =>
+                    SDIO <= sdio_inner(63);
+                    if(bit_cnt = bit_len - 1) then
+                        bit_cnt <= 0;    --ビットカウンタをリセット
+                        state   <= UPDATE;
+                    else
+                        bit_cnt    <= bit_cnt + 1;                      --ビットカウンタを1増やす
+                        sdio_inner <= sdio_inner(62 downto 0) & '0';    --1ビット左シフト
+                    end if;
 
-				--I/Oバッファから内部レジスタにデータを転送する
-				when UPDATE =>
-					CS				<= '1';	--送信完了
-					BUSY  		<= '0';	--ビジー状態から復帰
-					IO_UPDATE 	<= '1';	--更新
-					TXD			<= '1';	--送信完了を示す
-					state   		<= IDLE;
+                --I/Oバッファから内部レジスタにデータを転送する
+                when UPDATE =>
+                    CS        <= '1';    --送信完了
+                    BUSY      <= '0';    --ビジー状態から復帰
+                    IO_UPDATE <= '1';    --更新
+                    TXD       <= '1';    --送信完了を示す
+                    state     <= IDLE;
 
-				when others => null;
-			end case;
-		end if;
-	end process;
+                when others => null;
+            end case;
+        end if;
+    end process;
 end Behavioral;
--- ad9956_ver2/serial.vhd
+++ ad9956_ver2/serial.vhd
@@ -31,200 +31,236 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity serial is
-	Port ( 
-		CLK 			: in  	STD_LOGIC;	--入力クロック信号(32MHz)
-		RST 			: in  	STD_LOGIC;	--Papilioリセット信号(0でリセット，タクトスイッチで制御)
-		RQ				: in		STD_LOGIC;	--DDS読み書きリクエスト信号(0でリクエスト，タクトスイッチで制御)
-		R_OR_W		: in		STD_LOGIC;	--読み書き識別信号(0(ボタンを押す)で読み出し，1で書き込み，タクトスイッチで制御，とりあえず使用しない)
-		IO_RESET		: out		STD_LOGIC;	--9(1でリセット)
-		RESET			: out		STD_LOGIC;	--10(1でリセット)
-		SDO      	: in  	STD_LOGIC;	--13
-		SDIO 			: inout  STD_LOGIC;	--14
-      SCLK 			: inout 	STD_LOGIC;	--15(シリアル通信用クロック信号 1MHz)
-      CS 			: out  	STD_LOGIC;	--16(0で通信可能)
-      SYNC_IN		: out		STD_LOGIC;	--19(使用しない)
-		IO_UPDATE	: out		STD_LOGIC;	--20(1で更新)
-		PSEL			: inout	STD_LOGIC_VECTOR(2 downto 0);	--Profile Select(21 to 23)
-		TXD			: out		STD_LOGIC	--rs232c用(とりあえずledで代用，送信完了すると1になる)
-      );
+    Port ( 
+        CLK          : in       STD_LOGIC;    --入力クロック信号(32MHz)
+        RST          : in       STD_LOGIC;    --Papilioリセット信号(0でリセット，タクトスイッチで制御)
+        RQ           : in       STD_LOGIC;    --DDS読み書きリクエスト信号(0でリクエスト，タクトスイッチで制御)
+        R_OR_W       : in       STD_LOGIC;    --読み書き識別信号(0(ボタンを押す)で読み出し，1で書き込み，タクトスイッチで制御，とりあえず使用しない)
+        IO_RESET     : out      STD_LOGIC;    --9(1でリセット)
+        RESET        : out      STD_LOGIC;    --10(1でリセット)
+        SDO          : in       STD_LOGIC;    --13
+        SDIO         : inout    STD_LOGIC;    --14
+        SCLK         : inout    STD_LOGIC;    --15(シリアル通信用クロック信号 1MHz)
+        CS           : out      STD_LOGIC;    --16(0で通信可能)
+        SYNC_IN      : out      STD_LOGIC;    --19(使用しない)
+        IO_UPDATE    : out      STD_LOGIC;    --20(1で更新)
+        PSEL         : inout    STD_LOGIC_VECTOR(2 downto 0);    --Profile Select(21 to 23)
+        TXD          : out      STD_LOGIC     --rs232c用(とりあえずledで代用，送信完了すると1になる)
+    );
 end serial;
 
 architecture RTL of serial is
-	--Clock Generator
-	component clk_generator
-		port(
-			CLK_IN		: in  std_logic;	--入力クロック信号(32MHz)
-			SCLK			: out std_logic	--シリアル通信用クロック信号(1MHz)
-		);
-	end component;
+    --Clock Generator
+    component clk_generator
+        port(
+            CLK_IN    : in  std_logic;    --入力クロック信号(32MHz)
+            SCLK      : out std_logic     --シリアル通信用クロック信号(1MHz)
+        );
+    end component;
 
-	component dds_communication
-		port(
-			SCLK			: in		std_logic;								--シリアル通信用クロック信号(1MHz)
-			RST			: in		std_logic;								--リセット信号
-			RQ				: in		std_logic;								--リクエスト信号(1でリクエスト)
-			R_OR_W		: in		std_logic;								--読み書き識別信号(0で読み出し，1で書き込み，とりあえず使用しない)
-			AC_REG		: in		std_logic_vector(4 downto 0);		--アクセスレジスタ
-			DSEND			: in		std_logic_vector(63 downto 0);	--送信データ
-			SDIO			: inout	std_logic;								--通信信号
-			CS				: out		std_logic;								--チップセレクト信号(0で通信可能)
-			IO_UPDATE	: out		std_logic;								--データ更新信号(1で更新)
-			DGET			: out		std_logic_vector(63 downto 0);	--受信データ
-			BUSY			: out		std_logic;								--ビジー信号(1でビジー状態)
-			TXD			: out		std_logic
-		);
-	end component;
+    component dds_communication
+        port(
+            SCLK         : in       std_logic;                        --シリアル通信用クロック信号(1MHz)
+            RST          : in       std_logic;                        --リセット信号
+            RQ           : in       std_logic;                        --リクエスト信号(1でリクエスト)
+            R_OR_W       : in       std_logic;                        --読み書き識別信号(0で読み出し，1で書き込み，とりあえず使用しない)
+            AC_REG       : in       std_logic_vector(4 downto 0);     --アクセスレジスタ
+            DSEND        : in       std_logic_vector(63 downto 0);    --送信データ
+            SDIO         : inout    std_logic;                        --通信信号
+            CS           : out      std_logic;                        --チップセレクト信号(0で通信可能)
+            IO_UPDATE    : out      std_logic;                        --データ更新信号(1で更新)
+            DGET         : out      std_logic_vector(63 downto 0);    --受信データ
+            BUSY         : out      std_logic;                        --ビジー信号(1でビジー状態)
+            TXD          : out      std_logic                         --送信完了信号
+        );
+    end component;
 	
 	
-	-- state transition
-	type state_t is (IDLE, DDS_REQUEST, PC_REQUEST, STANDBY);
-	signal state : state_t := IDLE;
+    -- state transition
+    type state_t is (IDLE, DDS_REQUEST, PC_REQUEST, STANDBY);
+    signal state : state_t := IDLE;
 	
-	--inner signal
-	signal rq_dds		: std_logic := '0';	--DDS読み書きリクエスト信号(serial.vhdとdds_communication.vhdでのリクエスト信号は反転しているので注意)
-	signal rq_pc		: std_logic := '0';	--PC送信リクエスト信号
-	signal dsend		: std_logic_vector(63 downto 0)	:= (others => '0');
-	signal busy_dds	: std_logic;
+    --inner signal
+    signal rq_dds      : std_logic := '0';    --DDS読み書きリクエスト信号(serial.vhdとdds_communication.vhdでのリクエスト信号は反転しているので注意)
+    signal rq_pc       : std_logic := '0';    --PC送信リクエスト信号
+    signal dsend       : std_logic_vector(63 downto 0) := (others => '0');
+    signal busy_dds    : std_logic;
 	
-	--send data
-	signal ac_reg		: std_logic_vector(4 downto 0)	:= "00110";
-	signal pcr0			: std_logic_vector(63 downto 0)	:= "0000000000000000000000011101110010100000000111011100101100100000";
-	signal pcr1			: std_logic_vector(63 downto 0)	:= "0000000000000000000000111011100101000000001110111001001001011000";
-	signal pcr2			: std_logic_vector(63 downto 0)	:= "0000000000000000000001011001010111100000010110010101110101111000";
-	signal pcr3			: std_logic_vector(63 downto 0)	:= "0000000000000000000001110111001010000000011101110010100010011000";
-	signal pcr4			: std_logic_vector(63 downto 0)	:= "0000000000000000000010010100111100100000100101001111011110100000";
-	signal pcr5			: std_logic_vector(63 downto 0)	:= "0000000000000000000010110010101111000000101100101100001011000000";
-	signal pcr6			: std_logic_vector(63 downto 0)	:= "0000000000000000000011010000100001100000110100001000110111100000";
-	signal pcr7			: std_logic_vector(63 downto 0)	:= "0000000000000000000011101110010100000000111011100101100100000000";
+    --send data(後で外部アクセスできるようにするかも)
+    signal ac_reg    : std_logic_vector(4 downto 0)     := "00000";
+    signal cfr1      : std_logic_vector(31 downto 0)    := "00000000000000000000000000000000";
+    signal cfr2      : std_logic_vector(39 downto 0)    := "0000000000000000001110000000000000000111";
+    signal rdftw     : std_logic_vector(23 downto 0)    := "000000000000000000000000";    --使用しない
+    signal fdftw     : std_logic_vector(23 downto 0)    := "000000000000000000000000";    --使用しない
+    signal rsrr      : std_logic_vector(15 downto 0)    := "0000000000000000";            --使用しない
+    signal fsrr      : std_logic_vector(15 downto 0)    := "0000000000000000";            --使用しない
+    signal pcr0      : std_logic_vector(63 downto 0)    := "0000000000000000010010011101100010011101100010011101111010110110";
+    signal pcr1      : std_logic_vector(63 downto 0)    := "0000000000000000000000000011001001101001100011001111111100111010";
+    signal pcr2      : std_logic_vector(63 downto 0)    := "0000000000000000000000000100101110011110010100110111111011010111";
+    signal pcr3      : std_logic_vector(63 downto 0)    := "0000000000000000000000000110010011010011000110011111111001110101";
+    signal pcr4      : std_logic_vector(63 downto 0)    := "0000000000000000000000000111111000000111111000000111111000010010";
+    signal pcr5      : std_logic_vector(63 downto 0)    := "0000000000000000000010110010101111000000101100101100001011000000";
+    signal pcr6      : std_logic_vector(63 downto 0)    := "0000000000000000000011010000100001100000110100001000110111100000";
+    signal pcr7      : std_logic_vector(63 downto 0)    := "0000000000000000000011101110010100000000111011100101100100000000";
+
+    --動作確認用データ
+--    signal pcr0    : std_logic_vector(63 downto 0)	:= "0000000000000000000000011101110010100000000111011100101100100000";    --100kHz
+--    signal pcr1    : std_logic_vector(63 downto 0)	:= "0000000000000000000000111011100101000000001110111001001001011000";    --200kHz
+--    signal pcr2    : std_logic_vector(63 downto 0)	:= "0000000000000000000001011001010111100000010110010101110101111000";    --300kHz
+--    signal pcr3    : std_logic_vector(63 downto 0)	:= "0000000000000000000001110111001010000000011101110010100010011000";    --400kHz
+--    signal pcr4    : std_logic_vector(63 downto 0)	:= "0000000000000000000010010100111100100000100101001111011110100000";    --500kHz
+--    signal pcr5    : std_logic_vector(63 downto 0)	:= "0000000000000000000010110010101111000000101100101100001011000000";    --600kHz
+--    signal pcr6    : std_logic_vector(63 downto 0)	:= "0000000000000000000011010000100001100000110100001000110111100000";    --700kHz
+--    signal pcr7    : std_logic_vector(63 downto 0)	:= "0000000000000000000011101110010100000000111011100101100100000000";    --800kHz
 	
-	--clock
-	signal clk_inner_slow	: std_logic;
-	signal SCLK_slow			: std_logic;
-	signal cnt					: std_logic_vector(21 downto 0) := (others => '0');
-	signal cnt1					: std_logic_vector(12 downto 0) := (others => '0');
+    --clock
+    signal clk_inner_slow    : std_logic;
+    signal SCLK_slow         : std_logic;
+    signal cnt               : std_logic_vector(21 downto 0) := (others => '0');
+    signal cnt1              : std_logic_vector(12 downto 0) := (others => '0');
 	
 begin
-	--Clock Generator
-	clk_gen : clk_generator
-		port map(
-			CLK_IN		=> CLK,			--クロック信号(32MHz)
-			SCLK			=> SCLK			--シリアルクロック(1MHz)
+    --Clock Generator
+    clk_gen : clk_generator
+        port map(
+            CLK_IN    => CLK,    --クロック信号(32MHz)
+            SCLK      => SCLK    --シリアルクロック(1MHz)
+        );
+
+    dds_com : dds_communication
+        port map(
+            SCLK         => SCLK,
+            RST          => RST,
+            RQ           => rq_dds,
+            R_OR_W       => R_OR_W,
+            AC_REG       => ac_reg,
+            DSEND        => dsend,
+            SDIO         => SDIO,
+            CS           => CS,
+            IO_UPDATE    => IO_UPDATE,
+            DGET         => open,
+            BUSY         => busy_dds,
+            TXD          => TXD
 		);
 
-	dds_com : dds_communication
-		port map(
-			SCLK			=> SCLK,
-			RST			=> RST,
-			RQ				=> rq_dds,
-			R_OR_W		=> R_OR_W,
-			AC_REG		=> ac_reg,
-			DSEND			=> dsend,
-			SDIO			=> SDIO,
-			CS				=> CS,
-			IO_UPDATE	=> IO_UPDATE,
-			DGET			=> open,
-			BUSY			=> busy_dds,
-			TXD			=> TXD
-		);
-		
-	--シミュレーション用低速クロック
-	--25bit 1000000000000000000000000
-	--13bit 1000000000000
---	process(SCLK_slow) begin
---		if rising_edge(SCLK_slow) then
---			cnt1 <= cnt1 + '1';
---			if(cnt1 >= "1000000000000") then
---				SCLK <= '1';
---			else 
---				SCLK <= '0';
---			end if;
---		end if;
---	end process;
+    --シミュレーション用低速クロック
+    --25bit 1000000000000000000000000
+    --13bit 1000000000000
+--    process(SCLK_slow) begin
+--        if rising_edge(SCLK_slow) then
+--            cnt1 <= cnt1 + '1';
+--            if(cnt1 >= "1000000000000") then
+--                SCLK <= '1';
+--            else 
+--                SCLK <= '0';
+--            end if;
+--        end if;
+--    end process;
 	
-	process(SCLK, RST, RQ) begin
-		IO_RESET	<= '0';
-		RESET		<= '0';
+    process(SCLK, RST, RQ) begin
+        IO_RESET    <= '0';
+        RESET       <= '0';
 		
-		--初期化(起動時必ず行う)
-		if(RST = '0') then
-			IO_RESET		<= '1';
-			RESET			<= '1';
-			SYNC_IN		<= '0';
-			PSEL			<= "000";
-			ac_reg		<= "00110";
-			
-		elsif rising_edge(SCLK) then
-			case state is
-				when IDLE =>
-					dsend <= pcr0;
-					cnt	<= cnt + 1;
-					if(RQ = '0') then
-						PSEL		<= "000";
-						ac_reg	<= "00110";
-						cnt		<= (others => '0');
-						state		<= DDS_REQUEST;
+        --初期化(起動時必ず行う)
+        if(RST = '0') then
+            IO_RESET <= '1';
+            RESET    <= '1';
+            SYNC_IN  <= '0';
+            PSEL     <= "000";
+            ac_reg   <= "00000";
 
-					elsif(cnt = "1111111111111111111111") then
-						if(PSEL = "110") then
-							PSEL <= "000";
-						else
-							PSEL <= PSEL + 1;
-						end if;
-					end if;
+        elsif rising_edge(SCLK) then
+            case state is
+                when IDLE =>
+                    dsend <= cfr1 & (63-32 downto 0 => '0');
+                    cnt	  <= cnt + 1;
+                    if(RQ = '0') then
+                        PSEL   <= "000";
+                        ac_reg <= "00000";
+                        cnt    <= (others => '0');
+                        state  <= DDS_REQUEST;
+
+                    --レジスタの切り替え
+                    elsif(cnt = "1111111111111111111111") then
+                        --if(PSEL = "110") then
+                        if(PSEL = "000") then
+                            PSEL <= "000";
+                        else
+                            PSEL <= PSEL + 1;
+                        end if;
+                    end if;
 				
-				when DDS_REQUEST =>
-					--dds_communication.vhdにリクエスト信号を送る
-					--繰り返し送信するのを防ぐため，タクトスイッチを離した瞬間にリクエストを送るようにしている
-					if(RQ = '1') then
-						rq_dds <= '1';
-						state <= PC_REQUEST;
-					end if;
+                when DDS_REQUEST =>
+                    --dds_communication.vhdにリクエスト信号を送る
+                    --繰り返し送信するのを防ぐため，タクトスイッチを離した瞬間にリクエストを送るようにしている
+                    if(RQ = '1') then
+                        rq_dds <= '1';
+                        state  <= PC_REQUEST;
+                    end if;
 				
-				when PC_REQUEST =>
-					rq_dds <= '0';
-					if (busy_dds = '0' and rq_dds = '0') then
-						ac_reg <= ac_reg + 1;
-						
-						--送信データの更新(次回送信するデータ)
-						if (ac_reg = "00110") then
-							dsend <= pcr1;
-						elsif (ac_reg = "00111") then
-							dsend <= pcr2;
-						elsif (ac_reg = "01000") then
-							dsend <= pcr3;
-						elsif (ac_reg = "01001") then
-							dsend <= pcr4;
-						elsif (ac_reg = "01010") then
-							dsend <= pcr5;
-						elsif (ac_reg = "01011") then
-							dsend <= pcr6;
-						elsif (ac_reg = "01100") then
-							dsend <= pcr7;
-						else
-							dsend <= (others => '0');
-						end if;
-						
-						--アクセスレジスタが0x0Eになったら書き込み完了
-						if (ac_reg = "01101") then
-							--読み出しデータをPC上に表示するとき,リクエスト信号を送る，書き込みでは，すべてIDLE状態に遷移する
-							if(R_OR_W = '0') then
-								rq_pc <= '1';
-								state <= STANDBY;
-							else
-								state <= IDLE;
-							end if;
-						else
-							state <= DDS_REQUEST;
-						end if;
-					end if;
+                when PC_REQUEST =>
+                    rq_dds <= '0';
+                    if (busy_dds = '0' and rq_dds = '0') then					
+                        --送信データの更新(次回送信するデータ)
+                        if (ac_reg = "00000") then
+                            dsend <= cfr2 & (63-40 downto 0 => '0');
+                        elsif (ac_reg = "00001") then    --pcr0にジャンプ
+                            --dsend <= fdftw & (63-24 downto 0 => '0');
+                            dsend <= pcr0;
+                        elsif (ac_reg = "00010") then    --使用しない
+                            dsend <= fdftw & (63-24 downto 0 => '0');
+                        elsif (ac_reg = "00011") then    --使用しない
+                            dsend <= rsrr & (63-16 downto 0 => '0');
+                        elsif (ac_reg = "00100") then    --使用しない
+                            dsend <= fsrr & (63-16 downto 0 => '0');
+                        elsif (ac_reg = "00101") then    --使用しない
+                            dsend <= pcr0;
+                        elsif (ac_reg = "00110") then
+                            dsend <= pcr1;
+                        elsif (ac_reg = "00111") then
+                            dsend <= pcr2;
+                        elsif (ac_reg = "01000") then
+                            dsend <= pcr3;
+                        elsif (ac_reg = "01001") then
+                            dsend <= pcr4;
+                        elsif (ac_reg = "01010") then
+                            dsend <= pcr5;
+                        elsif (ac_reg = "01011") then
+                            dsend <= pcr6;
+                        elsif (ac_reg = "01100") then
+                            dsend <= pcr7;
+                        else
+                            dsend <= (others => '0');
+                        end if;
+
+                        --アクセスレジスタの更新(次回アクセスするレジスタ) RDFTW~FSRRは省略
+                        if (ac_reg = "00001") then
+                            ac_reg <= "00110";
+                        else
+                            ac_reg <= ac_reg + 1;
+                        end if;
+
+                        --アクセスレジスタが0x0Eになったら書き込み完了
+                        if (ac_reg = "01101") then
+                            --読み出しデータをPC上に表示するとき,リクエスト信号を送る，書き込みでは，すべてIDLE状態に遷移する
+                            if(R_OR_W = '0') then
+                                rq_pc <= '1';
+                                state <= STANDBY;
+                            else
+                                state <= IDLE;
+                            end if;
+                        else
+                            state <= DDS_REQUEST;
+                        end if;
+                    end if;
 					
-				--読み出しデータをPC上に表示するときに使用する，今回は使用しない
-				when STANDBY =>
-					rq_pc <= '0';
-					state <= IDLE;
-				
-				when others => null;
-			end case;
-		end if;
-	end process;
+                --読み出しデータをPC上に表示するときに使用する，今回は使用しない
+                when STANDBY =>
+                    rq_pc <= '0';
+                    state <= IDLE;
+
+                when others => null;
+            end case;
+        end if;
+    end process;
 end RTL;
